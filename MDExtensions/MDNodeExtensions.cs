05b6734d13ebb59d2941c4225f74d0628c8b3936
Beider <vegarste@gmail.com>
2020-07-10 10:08:04 +0200

    Fixed casing

--- MDExtensions/MDNodeExtensions.cs
+++ MDExtensions/MDNodeExtensions.cs
@@ -379,10 +379,10 @@ namespace MD
 
         public static bool Invoke(this Node Instance, String Method, params object[] Parameters)
         {
-            MethodInfo info = MDStatics.GetMethodInfo(Instance, Method, Parameters);
-            if (info != null)
+            MethodInfo Info = MDStatics.GetMethodInfo(Instance, Method, Parameters);
+            if (Info != null)
             {
-                info.Invoke(Instance, Parameters);
+                Info.Invoke(Instance, Parameters);
                 return true;
             }
 
@@ -391,10 +391,10 @@ namespace MD
 
         public static bool SetMemberValue(this Node Instance, String Name, object Value)
         {
-            MemberInfo member = MDStatics.GetMemberInfo(Instance, Name);
-            if (member != null)
+            MemberInfo Member = MDStatics.GetMemberInfo(Instance, Name);
+            if (Member != null)
             {
-                member.SetValue(Instance, Value);
+                Member.SetValue(Instance, Value);
                 return true;
             }
 
