4b093d4e655d4a4f4da552bd8558b9999ef939f7
Dylan Dumesnil <dadumesnil@gmail.com>
2020-09-20 21:11:51 -0700

    Add static methods to all bindingflags

--- MDHelpers/MDStatics.cs
+++ MDHelpers/MDStatics.cs
@@ -24,7 +24,7 @@ namespace MD
 
         private static readonly Dictionary<Type, IMDDataConverter> DataConverterTypeCache = new Dictionary<Type, IMDDataConverter>();
 
-        public static BindingFlags BindFlagsAll = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy;
+        public static BindingFlags BindFlagsAll = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy | BindingFlags.Static;
 
         // MDStatics needs a reference to a Godot object to really be useful, so the GameInstance sets a reference to itself here
         public static MDGameInstance GI { get; set; }
